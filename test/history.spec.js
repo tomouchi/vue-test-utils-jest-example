2c344b89d55e919eba215b608eee4d229a44f2ab
tomouchi <29200586+tomouchi@users.noreply.github.com>
2019-07-07 23:56:19 +0900

    window.historyをmockする説明を加えた

--- test/history.spec.js
+++ test/history.spec.js
@@ -1,11 +1,26 @@
 describe('window.history', function () {
-  it('history.length', () => {
-    expect(window.history.length).toBe(1);
-    window.history.pushState(null, null, 'url');
-    expect(window.history.length).toBe(2);
+  it('window.history.lengthが0であること', () => {
+
+    // 履歴を確認
+    console.log(window.history.length);
+
+    // 履歴を追加
+    window.history.pushState(null, null, null);
+
+    // 履歴が追加されていることを確認
+    console.log(window.history.length);
+
+    // window.history を Mockする
     jest.spyOn(window, 'history', 'get').mockImplementation(() => []);
+
+    // window.history.lengthが 0 であることを確認
+    console.log(window.history.length);
     expect(window.history.length).toBe(0);
+
+    // Mockを解除する
     jest.restoreAllMocks();
-    expect(window.history.length).toBe(2);
+
+    // Mockが解除されたことを確認
+    console.log(window.history.length);
   });
 });
